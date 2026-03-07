e06f6410e73211f0e0b9eedee5473bebe1f071ea
rodrigo <rodrigoalmeidafrancisco@gmail.com>
2026-03-07 20:55:32 -0300

    Testando effect

--- 01_FrontEnd/Angular/src/app/hello/hello.ts
+++ 01_FrontEnd/Angular/src/app/hello/hello.ts
@@ -1,4 +1,4 @@
-import { Component, computed, signal } from '@angular/core';
+import { Component, computed, effect, signal } from '@angular/core';
 
 @Component({
   selector: 'app-hello',
@@ -17,6 +17,7 @@ export class Hello {
 
   protected count = signal(0);
   protected doubleCount = computed(() => {   return this.count() * 2;  });
+  private readonly countLog = effect(() => { console.log('Count changed:', this.count()); });
 
   // protected getDoubleCount(): number {
   //   console.log('getDoubleCount called');
